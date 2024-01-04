c6c60ba6c05a08a286fdad97bfc2f532112ad487
valvava7 <154331360+valvava7@users.noreply.github.com>
2024-01-04 16:12:55 +0800

    complete extend.c

--- extend.c
+++ extend.c
@@ -1,4 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <wchar.h>
+#define N 256
 
+typedef struct questions_{
+    wchar_t problem[N];
+    wchar_t choice[4][N];
+    int answer;
+}Question;
+Question questions[N]; // all question
+int question_num = 0;
+void question_read(){
+    FILE *fp = fopen("./problem.txt", "r");
+    wchar_t line[N];
+    question_num = 0;
+    while(fgetws(line, sizeof(line), fp) != NULL){
+        swscanf(line, L"\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\",\"%[^\"]\"",
+            questions[question_num].problem,
+            questions[question_num].choice[0],
+            questions[question_num].choice[1],
+            questions[question_num].choice[2],
+            questions[question_num].choice[3]);
+            questions[question_num].answer = 0;
+        ++question_num;
+
+    }
+    fclose(fp);
+}
+void question_generate(Question *question){
+    int rd = rand()%question_num;
+    memcpy(question, &questions[rd], sizeof(wchar_t) * N);
+    question->answer = rand() % 4;
+    wchar_t temp[N];
+    wcscpy(temp, question->choice[question->answer]);
+    wcscpy(question->choice[question->answer], question->choice[0]);
+    wcscpy(question->choice[0], temp);
+}
+
+Question question_to_confirm[N];
+int question_to_confirm_num = 0;
+void question_to_confirm_add(Question question){
+    memcpy(question_to_confirm[question_to_confirm_num], question, sizeof(Question));
+    ++question_to_confirm;
+}
+
+
+
+//user's part
+typedef struct users_{
+    char id[N];
+    char pwd[N];
+    int coin;
+}User;
+User users[N];
+int user_num = 0;
+void user_read(){
     FILE *fp = fopen("./users.txt","r");
     char line[N*2];
     user_num = 0;
