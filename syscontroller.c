6ad23fe53a451f1d938411b2b6968dc864db6bfd
Wang <wangwin0705@gmail.com>
2024-01-11 09:46:20 +0800

    update

--- syscontroller.c
+++ syscontroller.c
@@ -97,7 +97,7 @@ int main(int argc, char **argv){
     sockfd = Socket(AF_INET, SOCK_STREAM, 0);
     bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT+3);
+	servaddr.sin_port = htons(SERV_PORT+2);
 	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 	func(stdin,sockfd);
